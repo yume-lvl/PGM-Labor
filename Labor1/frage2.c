93274942ef69c3548194d31274f07cd7d119c48e
Elvira <elviradicaro06@gmail.com>
2025-10-11 15:50:54 +0200

    Create frage2.c

---
+++ Labor1/frage2.c
@@ -0,0 +1,27 @@
+/*FRAGE 2: 
+Im folgenden Abschnitt sehen Sie ein Programm. Das Programm ist nicht lauffähig, weil es einen Syntax-Fehler enthält.
+
+Folgende Syntax-Regeln müssen Sie beachten:
+-C/C++ unterscheiden Groß- und Kleinschreibung.
+-Einzelne Worte werden durch Leerzeichen oder andere "Satzzeichen" getrennt.
+-Die diversen Klammern haben unterschiedliche Bedeutungen und treten alle paarweise (Klammer auf und zu) auf.
+-Anweisungen werden durch einen Strichpunkt abgeschlossen.
+-Verändern Sie das das folgende Programm, und Prüfen dann, ob es lauffähig wird.
+
+GEGEBEN: 
+#include <stdio.h>
+int main()
+{
+    printf("Hello World!\n")
+    return 0;
+}
+
+ERWARTETE AUSGABE:Hello World!*/
+
+
+//CODE:
+#include <stdio.h>
+int main()
+{
+    printf("Hello World!\n");
+    return 0;
