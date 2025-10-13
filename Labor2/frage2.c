165329859361c79774be7ea681aadd2fce50710d
Elvira <elviradicaro06@gmail.com>
2025-10-13 16:26:32 +0200

    Create frage2.c

---
+++ Labor2/frage2.c
@@ -0,0 +1,32 @@
+/*Gegeben sei folgendes Programm:
+
+#include <stdio.h>
+int main()
+{
+    int i = 10;
+    if (i == 20)
+        // printf("i ist 20. ");
+        printf("Programmende\n");
+    return 0;
+}
+
+Was wird ausgegeben? Bitte nur nachdenken, nicht ausprobieren!
+Select one:
+a. Ausgabe: "i ist 20. Programmende"
+b. Ausgabe: "Programmende"
+c. Ausgabe: "i ist 20. "
+d. Es wird kein Text ausgegeben*/
+
+
+//CODE:
+#include <stdio.h>
+int main()
+{
+    int i = 10;
+    if (i == 20)
+        // printf("i ist 20. ");
+        printf("Programmende\n");
+    return 0;
+}
+
+//ANTWORT: d. Es wird kein Text ausgegeben
