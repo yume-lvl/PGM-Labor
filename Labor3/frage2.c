497bc25c3bc46c4be35e08017efa116a927093c8
Elvira <elviradicaro06@gmail.com>
2025-10-20 14:49:19 +0200

    Create frage2.c

---
+++ Labor3/frage2.c
@@ -0,0 +1,23 @@
+/*FRAGE 2:
+Es sind drei Schleifen gegeben. Welche von diesen Schleifen werden als "Endlosschleifen" bezeichnet?
+
+-Schleife A
+while(1) 
+{ }
+
+-Schleife B
+for(;;) 
+{ }
+
+-Schleife C
+do
+{ }
+while(1);
+
+Bitte nur nachdenken, nicht ausprobieren!
+Select one:
+a. A und C
+b. Keine von diesen
+c. Alle drei Schleifen
+
+ANTWORT: c. Alle drei Schleifen*/
