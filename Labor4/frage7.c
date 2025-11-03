625d35a3b09b82650d6e8b2eb745abb398feb578
Elvira <elviradicaro06@gmail.com>
2025-11-03 17:09:51 +0100

    Create frage7.c

---
+++ Labor4/frage7.c
@@ -0,0 +1,21 @@
+/*FRAGE 7:
+Schreiben Sie eine Funktion triangleArea(), die den Flächeninhalt eines Dreiecks berechnet.
+Die Funktion bekommt als Parameter die Grundseite und die dazugehörige Höhe.
+
+Beispielhafter Prüfcode:
+-> printf("%.2f", triangleArea(3, 4));
+
+ERWARTETE AUSGABE:
+6.00*/
+
+//CODE:
+float triangleArea(float g, float h)
+{
+    return g * h * 1/2;
+}
+int main()
+{
+    printf("%.2f", triangleArea(3, 4));
+
+    return 0;
+}
