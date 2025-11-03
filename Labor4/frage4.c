29feb444a0cc90475ba50c291f9a18ad3b6f8a76
Elvira <elviradicaro06@gmail.com>
2025-11-03 15:35:55 +0100

    Create frage4.c

---
+++ Labor4/frage4.c
@@ -0,0 +1,21 @@
+/*FRAGE 4:
+Gegeben sei folgendes Programm:*/
+
+#include <stdio.h>
+int add(int a, int b) {
+    return  a + b;
+}
+int main() {
+    printf("Resultat: %d", add(19, 23) );
+    return 0;
+}
+
+/*Was kommt heraus? Bitte nur nachdenken, nicht ausprobieren!
+Select one:
+a. Resultat: 0
+b. Resultat: 21
+c. Programm stürzt ab
+d. Resultat: 42
+e. Programm startet nicht wg. Compiler
+
+ANTWORT: d. Resultat: 42*/
